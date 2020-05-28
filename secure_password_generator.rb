d9af63ca5d08673718bb60d0a260cda9a2553658
John Moons <j.moons@realdigitalmedia.com>
2020-05-28 13:18:10 -0400

    Randomizing the random number routine, snarky fun.

--- secure_password_generator.rb
+++ secure_password_generator.rb
@@ -1,3 +1,11 @@
+def randomize_random( input_collection_length )
+  random_index = nil
+  rand(100).times do |step|
+    random_index = rand( input_collection_length )
+  end
+  random_index
+end
+
 CHARACTER_SOURCE_SET = [
   ("a".."z").to_a,
   ("A".."Z").to_a,
@@ -10,10 +18,10 @@ secure_password    = ""
 
 (0 ... length_of_password).each do | step |
   # Randomly Determine which subset to use
-  subset_index = rand( CHARACTER_SOURCE_SET.length )
+  subset_index = randomize_random( CHARACTER_SOURCE_SET.length )
 
   # Randmonly determine which index to use from that source
-  subset_character_index = rand( CHARACTER_SOURCE_SET[subset_index].length )
+  subset_character_index = randomize_random( CHARACTER_SOURCE_SET[subset_index].length )
 
   # Grab the charcacter from the subset and append
   secure_password << CHARACTER_SOURCE_SET[subset_index][subset_character_index]
